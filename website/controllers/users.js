c88ea2bc2dca33fcf6baa693879c2de05668311a
Tona <carlostonatihu@gmail.com>
2015-10-10 19:54:57 -0500

    Mas

--- website/controllers/users.js
+++ website/controllers/users.js
@@ -32,10 +32,11 @@ users.prototype.get_main = function (req, res, next) {
     var self = this;
     this.model.getUser(req.user.id, function (data) {
       if(!data && data.complete === true) {
-        if(data.type === 1)
+        if(data.type === 1){
           self.view.main(res, object);
-        else
-          res.render('/flats/addflat');
+        }else{
+          res.redirect('/flats/addflat');
+        }
       } else {
         self.view.perfil(res, object);
       }
